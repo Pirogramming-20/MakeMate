52549317b54002d79ee20859db7a70180820a437
yeonuKim <ywk0524@snu.ac.kr>
2024-02-05 02:08:24 +0900

    [feat] AJAX를 활용한 그룹 생성 페이지 수정
    장고 전역변수를 자바스크립트 전역변수로 이동
    
    Resolves: #11

--- group/views.py
+++ group/views.py
@@ -5,67 +5,113 @@ from django.http import JsonResponse, HttpResponsePermanentRedirect
 from .forms import GroupBaseForm, GroupDetailForm, GroupDateForm
 from .models import Group
 
-new_group = Group()
-
 def group_base_info(request):
-    global new_group
     if request.method == 'POST':
         # request를 딕셔너리 형태로 변환 및 state 확인
-        data_dict = parse_qs(request.body.decode('utf-8'))
-        for key, values in data_dict.items():
-            data_dict[key] = values[0]
-        req = json.loads(json.dumps(data_dict))
-        state = int(req['state'])
+        request_dict = parse_qs(request.body.decode('utf-8'))
+        data_query = request_dict['cur_data'][0]
+        data_dict = parse_qs(data_query)
+        req = {key: values[0] for key, values in data_dict.items()}
         print(req)
+
+        # 이전 form 작성 정보가 있을 경우 prev_req로 저장
+        if 'prev_data' in request_dict:
+            prev_data_query = parse_qs(request.body.decode('utf-8'))['prev_data'][0]
+            prev_data_dict = parse_qs(prev_data_query)
+            prev_req = {key: values[0] for key, values in prev_data_dict.items()}
+            print(prev_req)
+        
+        # 현재 state 정보 저장
+        state = int(req['state'])      
 
         # state 0 == 첫번째 작성 내용 저장 및 두번째 작성내용 렌더링
         if state == 0:
             form = GroupBaseForm(data=req)
             if form.is_valid():
-                new_group.title = req['title']
-                new_group.team_number = int(req['team_number'])
-                new_group.password = req['password']
-                new_group.type = req['type']
+                title = req['title']
+                team_number = int(req['team_number'])
+                password = req['password']
+                type = req['type']
                 ctx = {
                     'form_html': GroupDetailForm().as_p(),
+                    'is_valid': True,
                     'state': 1,
+                    'prev_data': {'title': title,
+                                  'team_number': team_number,
+                                  'password': password,
+                                  'type': type},
                 }
                 return JsonResponse(ctx)
+            else: # non field 또는 field 에러 전송
+                ctx = {
+                    'state': 0,
+                    'is_valid': False,
+                    'errors': form.errors,
+                    'non_field_errors': form.non_field_errors(),
+                }
+                return JsonResponse(ctx)                
+
         
         # state 1 == 두번째 작성 내용 저장 및 세번째 작성내용 렌더링
-        elif state == 1:        
+        elif state == 1:
             form = GroupDetailForm(data=req)
             if form.is_valid():
-                if 'group_ability1' in req:
-                    new_group.group_ability1 = req['group_ability1']
-                if 'group_ability2' in req:
-                    new_group.group_ability2 = req['group_ability2']
-                if 'group_ability3' in req:
-                    new_group.group_ability3 = req['group_ability3']
-                if 'group_ability4' in req:
-                    new_group.group_ability4 = req['group_ability4']
-                if 'group_ability5' in req:
-                    new_group.group_ability5 = req['group_ability5']
-                new_group.choice = int(req['choice'])
-                new_group.tech_stack = req['tech_stack']
+                for idx in range(1, 6):
+                    prev_req[f'group_ability{idx}'] = req.get(f'ability_description{idx}', '')
+
+                prev_req['choice'] = int(req['choice'])
+                prev_req['tech_stack'] = req['tech_stack']
+
                 ctx = {
                     'form_html': GroupDateForm().as_p(),
+                    'is_valid': True,
                     'state': 2,
+                    'prev_data': prev_req,
                 }
                 return JsonResponse(ctx)
+            else: # non field 또는 field 에러 전송
+                ctx = {
+                    'state': 1,
+                    'is_valid': False,
+                    'errors': form.errors,
+                    'non_field_errors': form.non_field_errors(),
+                }
+                return JsonResponse(ctx) 
             
-        # state 2 == 세번째 작성 내용 저장 및 리디렉션
+        # state 2 == 세번째 작성 내용 저장 및 DB에 내용 저장
         elif state == 2:
             form = GroupDateForm(data=req)
             if form.is_valid():
-                new_group.end_date = form.cleaned_data['end_date']
+                prev_req['end_date'] = form.cleaned_data['end_date']
 
-                new_group.save()
+                Group.objects.create(
+                    title=prev_req['title'],
+                    team_number=prev_req['team_number'],
+                    password=prev_req['password'],
+                    type=prev_req['type'],
+                    ability_description1 = prev_req.get('group_ability1', ''),
+                    ability_description2 = prev_req.get('group_ability2', ''),
+                    ability_description3 = prev_req.get('group_ability3', ''),
+                    ability_description4 = prev_req.get('group_ability4', ''),
+                    ability_description5 = prev_req.get('group_ability5', ''),
+                    choice=prev_req['choice'],
+                    tech_stack=prev_req['tech_stack'],
+                    end_date=prev_req['end_date']
+                )
                 ctx = {
-                    'form_html': GroupDateForm().as_p(),
                     'state': 3,
+                    'is_valid': True,
+                    'prev_data': prev_req,
                 }
                 return JsonResponse(ctx)
+            else: # non field 또는 field 에러 전송
+                ctx = {
+                    'state': 2,
+                    'is_valid': False,
+                    'errors': form.errors,
+                    'non_field_errors': form.non_field_errors(),
+                }
+                return JsonResponse(ctx) 
 
     form = GroupBaseForm()
     ctx = {'form': form, 'state': 0}
--- static/js/group_info.js
+++ static/js/group_info.js
@@ -4,14 +4,100 @@ function getCookie(name) {
     if (parts.length === 2) return parts.pop().split(';').shift();
 }
 
-const changeStage = async (state) => {
+let state = 0;
+let prev_data;
+
+document.getElementById('base_set_submit_btn').addEventListener('click', async ()=>{
+    if (state == 0){
+        prev_data = await changeInitialStage(prev_data);
+    }
+    else if (state === 1 || state === 2) {
+        prev_data = await changeStage(state, prev_data);
+    }
+})
+
+function showErrors(response) {
+    const wrap_errors = document.getElementsByClassName('wrap_error');
+    const wrap_non_field_errors = document.getElementsByClassName('wrap_non_field_errors');
+        
+    // 기존 에러 메세지 지우기
+    Array.from(wrap_errors).forEach((target_div) => {
+        target_div.remove();
+    })
+
+    Array.from(wrap_non_field_errors).forEach((target_div) => {
+        target_div.remove();
+    })
+
+    // 필드 에러 메세지 띄우기
+    Object.keys(response.errors).forEach((key) => {
+        const target_input = document.getElementById(`id_${key}`);
+        
+        target_input.insertAdjacentHTML('afterend', `
+            <div class='wrap_error'>
+                <span>${response.errors[key]}</span>
+            </div>
+        `);
+    });
+
+    // 논필드 에러 메세지 띄우기
+    Object.keys(response.non_field_errors).forEach((key) => {
+        const target_input = document.querySelector(form);
+        
+        target_input.insertAdjacentHTML('afterend', `
+            <div class='wrap_non_field_errors'>
+                <p>${response.non_field_errors[key]}</p>
+            </div>
+        `);
+    });
+
+}
+
+function showDateErrors(response){
+    const wrap_errors = document.getElementsByClassName('wrap_error');
+    const wrap_non_field_errors = document.getElementsByClassName('wrap_non_field_errors');
+        
+    // 기존 에러 메세지 지우기
+    Array.from(wrap_errors).forEach((target_div) => {
+        target_div.remove();
+    })
+
+    Array.from(wrap_non_field_errors).forEach((target_div) => {
+        target_div.remove();
+    })
+
+    // 필드 에러 메세지 띄우기
+        const target_input = document.getElementById(`id_end_date_1`);
+        console.log(target_input)
+        console.log(response.errors)
+        const error_msg = response.errors[`end_date`];
+        
+        target_input.insertAdjacentHTML('afterend', `
+            <div class='wrap_error'>
+                <span>${error_msg}</span>
+            </div>
+        `);
+
+    // 논필드 에러 메세지 띄우기
+    Object.keys(response.non_field_errors).forEach((key) => {
+        const target_input = document.querySelector(form);
+        
+        target_input.insertAdjacentHTML('afterend', `
+            <div class='wrap_non_field_errors'>
+                <p>${response.non_field_errors[key]}</p>
+            </div>
+        `);
+    });
+}
+
+async function changeInitialStage(prev_data) {
     const url = '/group/base_set/';
     const formElement = document.querySelector('form');
     const formData = new FormData(formElement);
     const csrfToken = getCookie('csrftoken');
 
-    formData.append('state', state);
-    data = new URLSearchParams(formData).toString()
+    formData.set('state', state);
+    formdata = new URLSearchParams(formData).toString()
 
     const res = await fetch(url, {
         method: 'POST',
@@ -19,28 +105,67 @@ const changeStage = async (state) => {
             'Content-Type': 'application/x-www-form-urlencoded',
             'X-CSRFToken': csrfToken,
         },
-        body: data,
+        body: `cur_data=${encodeURIComponent(formdata)}`,
     });
 
     const response = await res.json();
-    console.log(response)
-    
-    if (response.state === 0) {
-        document.getElementById('state_desc').innerHTML = 'STEP 1. 기본 정보 작성';
-        document.getElementById('base_set_submit_btn').onclick = () => changeStage(0);
-    }
-    if (response.state === 1) {
+
+    if (response.is_valid) {
         document.getElementById('state_desc').innerHTML = 'STEP 2. 상세 설정';
         document.getElementById('form_container').innerHTML = response.form_html;
-        document.getElementById('base_set_submit_btn').onclick = () => changeStage(1);
+        state += 1;
+        const prevFormData = new URLSearchParams(response.prev_data).toString();
+
+        return prevFormData
     }
-    if (response.state === 2) {
-        document.getElementById('state_desc').innerHTML = 'STEP 3. 결과 발표 일정';
-        document.getElementById('form_container').innerHTML = response.form_html;
-        document.getElementById('base_set_submit_btn').onclick = () => changeStage(2);
+    else {
+        showErrors(response)
+        return prev_data
     }
-    if (response.state === 3) {
-        const redirect_url = '/group/share/';
-        window.location.href = redirect_url;
+} 
+
+async function changeStage(local_state, prev_data) {
+    const url = '/group/base_set/';
+    const formElement = document.querySelector('form');
+    const formData = new FormData(formElement);
+    const csrfToken = getCookie('csrftoken');
+    
+    formData.set('state', local_state);
+    formdata = new URLSearchParams(formData).toString()
+
+    const res = await fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'X-CSRFToken': csrfToken,
+        },
+        body: `cur_data=${encodeURIComponent(formdata)}&prev_data=${encodeURIComponent(prev_data)}`,
+    });
+
+    const response = await res.json();
+
+    if (response.is_valid){
+        if (local_state == 1) {
+            document.getElementById('state_desc').innerHTML = 'STEP 3. 결과 발표 일정';
+            document.getElementById('form_container').innerHTML = response.form_html;
+
+            state += 1;
+        }
+        else if (local_state == 2) {
+            const redirect_url = '/group/share/';
+            window.location.href = redirect_url;
+        }
+        const prevFormData = new URLSearchParams(response.prev_data).toString();
+        return prevFormData
+    }
+    else {
+        if (local_state == 1){
+            showErrors(response)
+        }
+        else {
+            showDateErrors(response)
+        }
+        return prev_data
     }
 }
+
