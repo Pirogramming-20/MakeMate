4be63070c5b58a9da87b4e2deed5bd6cf4e5bd88
kangyonghyeon <khyeon001003@gmail.com>
2024-02-10 21:18:55 +0900

    [feat]:member임시종료 페이지구현

--- group/views.py
+++ group/views.py
@@ -601,5 +601,23 @@ def result(request, group_id):
     return render(request, "group/result.html", context=ctx)
 
 
-def member_preresult(request):
-    pass
+def member_preresult(request, group_id):
+    group = Group.objects.get(id=group_id)
+    idea_list = Idea.objects.filter(
+        group=group).order_by("-score")[:group.team_number]
+    user_state = MemberState.objects.filter(user=request.user,
+                                            group=group).first()
+    ideas_votes = {}
+    if user_state:
+        ideas_votes["idea_vote1_id"] = (user_state.idea_vote1.id
+                                        if user_state.idea_vote1 else None)
+        ideas_votes["idea_vote2_id"] = (user_state.idea_vote2.id
+                                        if user_state.idea_vote2 else None)
+        ideas_votes["idea_vote3_id"] = (user_state.idea_vote3.id
+                                        if user_state.idea_vote3 else None)
+    ctx = {
+        "group": group,
+        "idea_list": idea_list,
+        "ideas_votes": ideas_votes,
+    }
+    return render(request, "preresult/preresult_member.html", ctx)
