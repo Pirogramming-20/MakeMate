6f056a96e1f866e71ab7bd2c74a365e08e712f4c
yeonuKim <ywk0524@snu.ac.kr>
2024-02-18 23:05:09 +0900

    [fix] 머지 후 충돌 수정

--- apps/preresult/views.py
+++ apps/preresult/views.py
@@ -239,12 +239,6 @@ def calculate_third_idea_scores(group_id):
 
 # preresult부분에서 calculate_idea_scores 함수를 호출하여
 # 특정 그룹의 모든 아이디어에 대한 점수를 계산하는 방식으로 생각했습니다.
-<<<<<<< HEAD
-# 아래는 예시
-
-=======
-
-
 def first_vote_preresult(request, group_id):
     group = Group.objects.get(id=group_id)
     if request.method == "POST":
@@ -282,4 +276,3 @@ def first_vote_unselect(request, group_id):
     idea.is_selected = False
     idea.save()
     return JsonResponse({"message": "good"})
->>>>>>> b32c93ae837d4907d07624ea4bf9ec45a642af54
